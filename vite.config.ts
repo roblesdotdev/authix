9585d96679ec0ae691c586602839213132b6face
roblesdotdev <httpsnip@gmail.com>
2024-09-19 18:26:03 -0300

    setup flat routes & dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
 import { vercelPreset } from '@vercel/remix/vite'
+import { remixDevTools } from 'remix-development-tools'
+import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 installGlobals()
 
 export default defineConfig({
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [
+    remixDevTools(),
+    remix({
+      presets: [vercelPreset()],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+      routes: async defineRoutes => {
+        return flatRoutes('routes', defineRoutes, {
+          ignoredRouteFiles: [
+            '.*',
+            '**/*.css',
+            '**/*.test.{js,jsx,ts,tsx}',
+            '**/__*.*',
+            // This is for server-side utilities you want to colocate
+            // next to your routes without making an additional
+            // directory. If you need a route that includes "server" or
+            // "client" in the filename, use the escape brackets like:
+            // my-route.[server].tsx
+            '**/*.server.*',
+            '**/*.client.*',
+          ],
+        })
+      },
+    }),
+    tsconfigPaths(),
+  ],
 })
